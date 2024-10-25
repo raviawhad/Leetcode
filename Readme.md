a3f7bed4bf9b923bd24b8e71a8cd8b38fe412666
raviawhad <raviawhad143@gmail.com>
2024-10-25 19:08:46 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+In this code I Have Done Revese string using string buffer 
+1. input = "Ravi";
+2. output = "ivaR";
+3. this  test Case has been passed successFuly;
---
+++ ReveseString.java
@@ -0,0 +1,13 @@
+public class ReveseString {
+
+    
+        public static void main(String[] args) {
+
+            String  str = "Ravi Awhad";
+            StringBuffer sb = new StringBuffer(str);
+             sb.reverse();
+
+            System.out.println(sb);
+        }
+    }
+
